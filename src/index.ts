941d9c94e06c4a96583e55134e235b7b7deb0088
davawen <arthaudawen.26@gmail.com>
2021-05-21 00:17:15 +0200

    Add color support and better memory management

--- src/index.ts
+++ src/index.ts
@@ -5,111 +5,139 @@ import * as fsExtra from "fs-extra";
 import { spawn, ChildProcessWithoutNullStreams } from 'child_process';
 
 import * as prompt from 'prompt';
-import * as draftlog from 'draftlog';
+import * as DraftLog from 'draftlog';
+import { Instance as Chalk } from "chalk";
 
 import * as playSound from 'play-sound';
 
-draftlog.into(console).addLineListener(process.stdin);
+//#region Set import configs
+const chalk = new Chalk( { level: 3 } );
+
+(prompt as any).colors = false;
+(prompt as any).message = "";
+
+DraftLog.into(console).addLineListener(process.stdin);
+
+//#endregion
 
 type PixelData = { data: Uint8Array, width: number, height: number; };
+let shading = " .:-=+*#%@".split("");
 
-interface Canvas
+class Canvas
 {
+	private frames: string[];
+
 	index: number;
-	frames: string[];
+	frameIndex: number;
 	finished: boolean;
-}
-
-interface Config
-{
-	ffmpegPath: string;
-	mplayerPath: string;
-}
-
-//#region Load config
-
-let config: Config =
-{
-	ffmpegPath: "",
-	mplayerPath: ""
-};
-
-if(fs.existsSync("./config.json"))
-{
-	config = JSON.parse(fs.readFileSync("./config.json", { encoding: "ascii" }));
-}
-
-//If empty, assume it's in the PATH
-if(config.ffmpegPath === "") config.ffmpegPath = "ffmpeg";
-else
-{
-	if(!fs.existsSync(config.ffmpegPath))
+	
+	constructor()
 	{
-		console.log("Error: Path to ffmpeg doesn't exists!"); //Note this doesn't give confirmation that the path is right
-		process.exit(1);
+		this.frameIndex = 0;
+		this.index = 0;
+		this.frames = [];
+		this.finished = false;
 	}
-}
-
-if(config.mplayerPath === "") config.mplayerPath = "mplayer";
-else
-{
-	if(!fs.existsSync(config.mplayerPath))
+	
+	popFrame(): string
 	{
-		console.log("Error: Path to mplayer doesn't exists!");
-		process.exit(1);
+		this.index--;
+		
+		return this.frames.pop();
+	}
+	
+	async extractFrames(ffmpegProcess: ChildProcessWithoutNullStreams): Promise<void>
+	{
+		for(let i = 9; i >= 0; i--) //Process ten frames at a time
+		{
+			this.frames.unshift("");
+			
+			let imagePath = `${__dirname}/ResizedFrames/out-${this.frameIndex + 1}.png`;
+
+			while(!fs.existsSync(imagePath))
+			{
+				if(ffmpegProcess.killed) //If reached the end of video and file doesn't exists
+				{
+					this.finished = true;
+					return;
+				}
+
+				await sleep(500); //Else wait for ffmpeg to catch up
+			}
+
+			let pixelData: PixelData = await getPixels(imagePath);
+
+			this.index++;
+			this.frameIndex++;
+
+			for(let j = 0; j < pixelData.data.length; j += 4)
+			{
+				let value = (.3 * pixelData.data[j] + .59 * pixelData.data[j + 1] + .11 * pixelData.data[j + 2]) / 256 * (pixelData.data[j + 3] / 255); // Get greyscale value then multiply everything by alpha
+				
+				this.frames[0] += shading[Math.floor(value * value * shading.length)];
+				
+				if(j/4 % pixelData.width >= pixelData.width - 1) this.frames[0] += "\n";
+			}
+		}
+		
+		while(this.index >= 500) // Don't hold a lot of frames in memory
+		{
+			await sleep(300);
+		}
+		
+		this.extractFrames(ffmpegProcess);
+	}
+	
+	async extractFramesColor(ffmpegProcess: ChildProcessWithoutNullStreams): Promise<void>
+	{
+		for(let i = 9; i >= 0; i--) //Process ten frames at a time
+		{
+			this.frames.unshift("");
+			
+			let imagePath = `${__dirname}/ResizedFrames/out-${this.frameIndex + 1}.png`;
+
+			while(!fs.existsSync(imagePath))
+			{
+				if(ffmpegProcess.killed) //If reached the end of video and file doesn't exists
+				{
+					this.finished = true;
+					return;
+				}
+
+				await sleep(500); //Else wait for ffmpeg to catch up
+			}
+
+			let pixelData: PixelData = await getPixels(imagePath);
+
+			this.index++;
+			this.frameIndex++;
+
+			for(let j = 0; j < pixelData.data.length; j += 4)
+			{
+				this.frames[0] += chalk.rgb(pixelData.data[j], pixelData.data[j + 1], pixelData.data[j + 2])("█"); // █
+
+				if(j/4 % pixelData.width >= pixelData.width - 1) this.frames[0] += "\n";
+			}
+		}
+
+		while(this.index >= 500) // Don't hold a lot of frames in memory
+		{
+			await sleep(500);
+		}
+
+		this.extractFramesColor(ffmpegProcess);
 	}
 }
 
 const audioPlayer = playSound(
 	{
-		player: config.mplayerPath
+		player: "mplayer"
 	}
 );
-
-//#endregion
 
 async function sleep(time: number)
 {
 	return new Promise(resolve => setTimeout(resolve, time));
-}
-
-async function extractFrames(canvas: Canvas, ffmpegProcess: ChildProcessWithoutNullStreams)
-{
-	let shading = " .:-=+*#%@".split("");
-	
-	for(let i = 0; i < 10; i++) //Process ten frames at a time
-	{
-		let imagePath = `${__dirname}/ResizedFrames/out-${canvas.index + 1}.png`;
-		
-		while(!fs.existsSync(imagePath))
-		{
-			if(ffmpegProcess.killed) //If reached the end of video and file doesn't exists
-			{
-				canvas.finished = true;
-				return;
-			}
-			
-			await sleep(500); //Else wait for ffmpeg to catch up
-		}
-		
-		let pixelData: PixelData = await getPixels(imagePath);
-		canvas.index++;
-		
-		let str = "";
-
-		for(let j = 0; j < pixelData.data.length; j += 4)
-		{
-			let value = (.3 * pixelData.data[j] + .59 * pixelData.data[j + 1] + .11 * pixelData.data[j + 2]) / 256 * (pixelData.data[j + 3] / 255); // Get greyscale value then multiply everything by alpha
-			
-			str += shading[Math.floor(value * value * shading.length)];
-			
-			if(Math.floor(j/4) % pixelData.width == pixelData.width-1) str += "\n";
-		}
-
-		canvas.frames.push(str);
-	}
-	
-	extractFrames(canvas, ffmpegProcess);
 }
 
 prompt.start();
@@ -120,14 +148,15 @@ prompt.get(
 			name: "videoPath",
 			description: "Path to the video file",
 			allowEmpty: false,
-			pattern: /.+\.((mp4)|(webm)|(mpg)|(mpeg)|(m4v)|(avi)|(mov)|(qt)|(flv)|(ogg)|(wmv)|(mkv))/
+			conform: (value) => fs.existsSync(value),
+			message: "Path must exist."
 		},
 		{
-			name: "width",
-			description: "Video width in characters",
-			default: 120,
+			name: "color",
+			description: "Wether color should be used (y/n) (Not recommended with a large width)",
+			default: "n",
 			allowEmpty: false,
-			pattern: /[0-9]+/
+			pattern: /[yYnN]/
 		},
 		{
 			name: "audio",
@@ -135,6 +164,13 @@ prompt.get(
 			default: "y",
 			allowEmpty: false,
 			pattern: /[yYnN]/
+		},
+		{
+			name: "width",
+			description: "Video width in characters (Fractional amount will scale based on terminal size)",
+			default: 120,
+			allowEmpty: false,
+			pattern: /[0-9]+/
 		}
 	],
 	(err, result) =>
@@ -154,27 +190,64 @@ prompt.get(
 		const splitPath = videoPath.split(/\/|\\/);
 		const video = splitPath[splitPath.length-1];
 		
-		const videoWidth = result["width"];
+		let videoWidth = parseFloat(result["width"] as string);
 		
 		const isPlayingAudio = /[yY]/.test(result["audio"] as string);
+		const isUsingColor = /[yY]/.test(result["color"] as string);
 		
-		let ffmpegFps = spawn('ffmpeg', ['-i', videoPath]);
-		let fpsString = '';
+		let ffmpegMetadata = spawn('ffmpeg', ['-i', videoPath]);
+		let videoMetadata = '';
 		let fps = 0;
+		let width = 0;
+		let height = 0;
 		
-		ffmpegFps.stderr.on('data',
+		ffmpegMetadata.stderr.on('data',
 			(data) =>
 			{
-				fpsString += data;
+				videoMetadata += data;
 			}
 		);
 		
 		//#endregion
 		
-		ffmpegFps.on('close',
+		ffmpegMetadata.on('close',
 			async () =>
 			{
-				fps = parseInt(fpsString.split("fps")[0].split("kb/s, ")[1]);
+				if(videoMetadata.includes("Invalid data found when processing input"))
+				{
+					console.log("Input file is not a valid file format.");
+					process.exit(1);
+				}
+				
+				fps = parseFloat(videoMetadata.match(/[0-9\.]+ fps/)[0]);
+				
+				//Search for 1920x1080 but omit 0x181254 for exemple
+				let size = videoMetadata.match(/(?:(?!0{1})[0-9]+)x[0-9]+/g)[0].split("x");
+				width = parseFloat(size[0]);
+				height = parseFloat(size[1])/2;
+				
+				if(videoWidth <= 1) //Fractional unit
+				{
+					videoWidth = Math.floor(process.stdout.columns * videoWidth);
+				}
+				
+				let videoHeight = videoWidth / (width/height);
+
+				
+				// Resize if too big for console
+				if(videoHeight > process.stdout.rows - 4)
+				{
+					videoHeight = process.stdout.rows - 4;
+					
+					videoWidth = videoWidth / ((videoWidth / videoHeight) / (width / height))
+				}
+				
+				if(videoWidth > process.stdout.columns)
+				{
+					videoWidth = process.stdout.columns;
+					
+					videoHeight = videoHeight * ((videoWidth / videoHeight) / (width / height))
+				}
 				
 				fsExtra.emptyDirSync(`${__dirname}/ResizedFrames`);
 				
@@ -186,61 +259,65 @@ prompt.get(
 				);
 				
 				//Reduce aspect ratio by 2
-				let ffmpeg = spawn(config.ffmpegPath, ['-i', videoPath, '-vf', `scale=${videoWidth}:(((${videoWidth}/iw)/2))*ih`, `${__dirname}/ResizedFrames/out-%d.png`]);
+				let ffmpeg = spawn("ffmpeg", ['-i', videoPath, '-vf', `scale=${videoWidth}:${videoHeight}`, `${__dirname}/ResizedFrames/out-%d.png`]);
 				
 				process.stdout.write("\u001b[2J\u001b[0;0H"); //Clear terminal
 				
 				await sleep(1000); //Wait for ffmpeg to warm up
 				
-				let canvas: Canvas = 
-				{
-					frames: [],
-					index: 0,
-					finished: false
-				};
+				let canvas: Canvas = new Canvas();
 				
-				extractFrames(canvas, ffmpeg);
+				if(isUsingColor) canvas.extractFramesColor(ffmpeg);
+				else canvas.extractFrames(ffmpeg);
 				
-				
-				console.log(`Playing ${video} !`);
+				let firstFrame: PixelData = await getPixels(`${__dirname}/ResizedFrames/out-1.png`);
+
+				console.log(`Playing ${video} in ${firstFrame.width}x${firstFrame.height * 2} at ${fps} FPS !`);
 				
 				await sleep(2000); //Wait for processing to warm up
 				
-				let currentFrame = console.draft(canvas.frames[0]);
-				let newFrame = canvas.frames[0];
+				let frame = console.draft(canvas.popFrame());
 				
 				// Play audio
 				if(isPlayingAudio) audioPlayer.play(`${__dirname}/ResizedFrames/audio.mp3`,
-					(err: Error) => 
+					(err) =>
 					{
-						process.stdout.write("\u001b[2J\u001b[0;0H");
-
-						process.exit(0);
+						if(err)
+						{
+							console.log(err);
+							
+							process.exit(1);
+						}
 					}
 				);
 				
 				let index = 0;
-				let start = Date.now();
+				let lastUpdate = Date.now();
 				
 				const updateDelay = 1000 / fps;
 				
-				while(!canvas.finished || index < canvas.index)
+				while(true)
 				{
-					let frameIndex = Math.floor(index)
-					
-					let frame = canvas.frames[frameIndex];
-					
-					if(frame == undefined)
+					if(canvas.index <= 0 && canvas.finished)
 					{
-						await sleep(updateDelay);
-						continue;
+						console.log(`\nFinished playing ${video}!`);
+
+						process.exit(0);
 					}
 					
-					canvas.frames[frameIndex] = ""; // Reduce memory usage
+					if(Date.now() - lastUpdate < updateDelay)
+					{
+						await sleep(updateDelay - (Date.now() - lastUpdate));
+					}
 					
-					currentFrame("\n" + frame);
+					lastUpdate = Date.now();
 					
-					index = (Date.now() - start) / updateDelay;
+					let currentFrame = canvas.popFrame();
+					
+					if(currentFrame != undefined)
+					{
+						frame("\n" + currentFrame);
+					}
 				}
 			}
 		);
